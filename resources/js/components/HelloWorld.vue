75076512e9a9876d64e02fb1acbe995e7fba5c4a
sam2904 <sudhanshu.mittal@deorwine.com>
2021-11-15 13:58:06 +0530

    setup

---
+++ resources/js/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>{{ greeting }}</h1>
+  <h2>{{ message }}</h2>
+  <h3 style="font-family: bold; color: red">Yeah!!!</h3>
+</template>
+<script>
+export default {
+  setup: () => ({
+    greeting: "Hello World from Vue 3!",
+    message: "Your techSam is here for you!",
+  }),
+};
+</script>
